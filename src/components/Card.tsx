d8185e3d3a1b2837d5e1df5a9fc36628d5bf72ce
Bagas Dhitya Taufiqqi <bagas@Bagass-MacBook-Air.local>
2023-03-01 16:45:22 +0700

    restyle card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,20 +5,22 @@ import { BsPencilFill, BsTrash } from "react-icons/bs";
 interface CardProps {
   id: string;
   title?: string;
+  date?: string;
   onUpdate?: React.MouseEventHandler;
   onDelete?: React.MouseEventHandler;
 }
 
-const Card: FC<CardProps> = ({ id, title, onUpdate, onDelete }) => {
+const Card: FC<CardProps> = ({ id, title, date, onUpdate, onDelete }) => {
   return (
     <div
       id={id}
-      className="flex flex-row justify-between w-full h-16 outline outline-offset-2 outline-2 outline-todoist-indigo rounded-md shadow-md"
+      className="flex flex-row justify-between w-full h-20 outline outline-offset-2 outline-2 outline-todoist-indigo rounded-md shadow-md"
     >
-      <div className="grid content-center">
+      <div className="-space-y-5">
         <p className="font-semibold text-xl text-todoist-indigo m-5">{title}</p>
+        <p className="text-md text-todoist-indigo m-5">{date}</p>
       </div>
-      <div className="m-6 flex flex-row space-x-3">
+      <div className="m-10 flex flex-row space-x-3">
         <IconContext.Provider value={{ color: "#4445C4" }}>
           <BsPencilFill onClick={onUpdate} />
           <BsTrash onClick={onDelete} />
