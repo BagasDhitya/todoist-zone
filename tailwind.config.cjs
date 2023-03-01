77af33372f9e10260fc28c4194fc79ad5dc8fac5
Bagas Dhitya Taufiqqi <bagas@Bagass-MacBook-Air.local>
2023-03-01 15:04:50 +0700

    custom theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "todoist-indigo": "#4445C4",
+        "todoist-iron": "#EAEAEB",
+        "todoist-blue-marguerite": "#746CC5",
+        "todoist-boulder": "#747474",
+      },
+    },
   },
   plugins: [],
 };
